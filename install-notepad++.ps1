b729ad035f8ecbf742928af703ea91a0bf73e89c
Shruti-git-007 <77736903+Shruti-git-007@users.noreply.github.com>
2023-08-09 23:17:18 +0530

    Update install-notepad++.ps1

--- install-notepad++.ps1
+++ install-notepad++.ps1
@@ -1,28 +1,30 @@
-# Download from the website
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$InstallPackage
+)
+
+# Test: Download from the website
 $DownloadUrl = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.5.4/npp.8.5.4.Installer.x64.exe" 
 
 # Define the Cohesity share folder path and the local temporary folder path
-#$cohesitySharePath = "\\<CohesityShare>\<Path>"
+#$cohesitySharePath = "\\10.229.208.24\appsshare"
 #$Username = "<username>"
 #$Password = ConvertTo-SecureString "<password>" -AsPlainText -Force
 #$Credential = New-Object System.Management.Automation.PSCredential ($Username, $Password)
 
-#$LocalTempPath = "$env:TEMP\CohesityInstall"
+# Define the local temporary folder 
 $LocalTempPath = [Environment]::GetFolderPath('Desktop') + "\tmp\"
 # Create the temporary folder if it doesn't exist
-#New-Item -ItemType Directory -Path $LocalTempPath -ErrorAction SilentlyContinue | Out-Null
 if (!(Test-Path -Path $LocalTempPath)) {
     New-Item -ItemType Directory -Force -Path $LocalTempPath
 }
-$InstallPackage = "npp.Installer.exe" 
 $LocalInstallPath = $LocalTempPath + $InstallPackage
 
+# Test: Download the installer
+Invoke-WebRequest -Uri $DownloadUrl -OutFile $LocalInstallPath
 
 # Copy the install package from the Cohesity share folder to the temporary folder
 #Copy-Item -Path "$cohesitySharePath\$InstallPackage" -Destination $LocalInstallPath -Credential $Credential
-
-# Download the installer
-Invoke-WebRequest -Uri $DownloadUrl -OutFile $LocalInstallPath
 
 # Check if the installer executable exists in the temporary folder
 if (Test-Path $LocalInstallPath -PathType Leaf) {
